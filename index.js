648f11517465cb3867fc29faddc2d854e780dce0
Gabriel Oriente <gabriel.oriente@mirumagency.com>
2018-07-14 23:44:39 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const restify = require('restify');
+
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : '',
+    database : 'nodedb'
+  }
+});
+
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get('/echo/:name', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
